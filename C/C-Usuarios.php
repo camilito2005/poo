80cd10e3383cfc628c663188ca45fa5b20ac34bc
camilo Marrugo Barrios <marrugobarrioscamilo2005@gmail.com>
2024-06-26 16:17:06 -0500

    terminando

--- C/C-Usuarios.php
+++ C/C-Usuarios.php
@@ -24,7 +24,7 @@ class Registro {
                 $this->editUser();
                 break;
             case 'update':
-                $this->Crear();
+                $this->editUser();
                 break;
             case 'delete':
                 $this->deleteUser();
--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
 $DB_HOST=('localhost');
-$DB_NAME=('nombre_base_de_datos');
-$DB_USER=('usuario');
-$DB_PASS=('contraseña');
+$DB_NAME=('poo');
+$DB_USER=('root');
+$DB_PASS=('');
 
 try {
     $pdo = new PDO("mysql:host=" . $DB_HOST . ";dbname=" . $DB_NAME, $DB_USER, $DB_PASS);
